4e669e89877f7e0cde1419c3d227f21253a22d1e
Lard Corporation <jl.lard.2018@gmail.com>
2025-06-23 19:07:31 -0600

    added old guides

---
+++ old/guides/basic/code.js
@@ -0,0 +1,12 @@
+function main() {
+    window.location.href = "../index.html";
+    }
+function basic() {
+    window.location.href = "../basic.html";
+    }
+function dindex() {
+    window.location.href = "dictionary/index.html";
+    }
+function index() {
+    window.location.href = "index.html";
+    }
---
+++ old/guides/code.js
@@ -0,0 +1,18 @@
+function main() {
+    window.location.href = "https://www.lardcorporation.uk.eu.org";
+    }
+function basic() {
+    window.location.href = "basic.html";
+    }
+function dindex() {
+    window.location.href = "dictionary/index.html";
+    }
+function index() {
+    window.location.href = "index.html";
+    }
+function intermediate() {
+    window.location.href = "intermediate.html";
+    }
+function advanced() {
+    window.location.href = "advanced.html";
+    }
